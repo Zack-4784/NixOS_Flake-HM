a0b38ffb347d0beab1280ba448a82361297c9b17
Zack-4784 <trydster@hotmail.com>
2026-04-01 11:08:21 +0100

    AUTOMATED REBUILD

--- Home-Manager/Program-Configurations/Firefox/firefox.nix
+++ Home-Manager/Program-Configurations/Firefox/firefox.nix
@@ -1,33 +1,51 @@
 { config, pkgs, lib, ... }:
+
+let
+  template = "${config.home.homeDirectory}/documents/NixOS/Home-Manager/Program-Configurations/Firefox/Firefox-Files-Template";
+  profile  = "${config.home.homeDirectory}/.mozilla/firefox/s0u782un.default";
+in
 {
+  # -------------------------
   # Core config files
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/prefs.js".source = "/home/zack/.mozilla/firefox/s0u782un.default/prefs.js";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/xulstore.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/xulstore.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/handlers.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/handlers.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/containers.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/containers.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/permissions.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/permissions.sqlite";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/search.json.mozlz4".source = "/home/zack/.mozilla/firefox/s0u782un.default/search.json.mozlz4";
+  # -------------------------
+  home.file."${profile}/prefs.js".source = "${template}/prefs.js";
+  home.file."${profile}/xulstore.json".source = "${template}/xulstore.json";
+  home.file."${profile}/handlers.json".source = "${template}/handlers.json";
+  home.file."${profile}/containers.json".source = "${template}/containers.json";
+  home.file."${profile}/permissions.sqlite".source = "${template}/permissions.sqlite";
+  home.file."${profile}/search.json.mozlz4".source = "${template}/search.json.mozlz4";
 
+  # -------------------------
   # Extensions
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/extensions".source = "/home/zack/.mozilla/firefox/s0u782un.default/extensions";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/extension-settings.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/extension-settings.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/extensions.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/extensions.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/extension-preferences.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/extension-preferences.json";
+  # -------------------------
+  home.file."${profile}/extensions".source = "${template}/extensions";
+  home.file."${profile}/extensions".recursive = true;
 
+  home.file."${profile}/extension-settings.json".source = "${template}/extension-settings.json";
+  home.file."${profile}/extensions.json".source = "${template}/extensions.json";
+  home.file."${profile}/extension-preferences.json".source = "${template}/extension-preferences.json";
+
+  # -------------------------
   # Bookmarks + history
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/places.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/places.sqlite";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/favicons.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/favicons.sqlite";
+  # -------------------------
+  home.file."${profile}/places.sqlite".source = "${template}/places.sqlite";
+  home.file."${profile}/favicons.sqlite".source = "${template}/favicons.sqlite";
 
+  # -------------------------
   # Passwords
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/logins.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/logins.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/logins-backup.json".source = "/home/zack/.mozilla/firefox/s0u782un.default/logins-backup.json";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/key4.db".source = "/home/zack/.mozilla/firefox/s0u782un.default/key4.db";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/cert9.db".source = "/home/zack/.mozilla/firefox/s0u782un.default/cert9.db";
+  # -------------------------
+  home.file."${profile}/logins.json".source = "${template}/logins.json";
+  home.file."${profile}/logins-backup.json".source = "${template}/logins-backup.json";
+  home.file."${profile}/key4.db".source = "${template}/key4.db";
+  home.file."${profile}/cert9.db".source = "${template}/cert9.db";
 
+  # -------------------------
   # Cookies + site storage
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/cookies.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/cookies.sqlite";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/webappstore.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/webappstore.sqlite";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/storage/ls-archive.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/ls-archive.sqlite";
-  home.file."/home/zack/documents/NixOS/Home-Manager/Program-Configuratoins/Firefox/Firefox-Files-Template/storage.sqlite".source = "/home/zack/.mozilla/firefox/s0u782un.default/storage.sqlite";
-}
+  # -------------------------
+  home.file."${profile}/cookies.sqlite".source = "${template}/cookies.sqlite";
+  home.file."${profile}/webappstore.sqlite".source = "${template}/webappstore.sqlite";
 
+  # Only safe persistent storage files
+  home.file."${profile}/ls-archive.sqlite".source = "${template}/ls-archive.sqlite";
+  home.file."${profile}/storage.sqlite".source = "${template}/storage.sqlite";
+}
