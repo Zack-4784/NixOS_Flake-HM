7b515161938c28e440e0bd60c9b06b2a68954567
Zack-4784 <trydster@hotmail.com>
2026-04-01 13:29:52 +0100

    AUTOMATED REBUILD

--- Home-Manager/Program-Configurations/Firefox/firefox.nix
+++ Home-Manager/Program-Configurations/Firefox/firefox.nix
@@ -1,13 +1,10 @@
 { config, pkgs, lib, ... }:
 
 let
-  template = "${config.home.homeDirectory}./Firefox-Files-Template";
+  template = ./Firefox-Files-Template;
   profile  = "${config.home.homeDirectory}/.mozilla/firefox/s0u782un.default";
 in
 {
-  # -------------------------
-  # Core config files
-  # -------------------------
   home.file."${profile}/prefs.js".source = "${template}/prefs.js";
   home.file."${profile}/xulstore.json".source = "${template}/xulstore.json";
   home.file."${profile}/handlers.json".source = "${template}/handlers.json";
@@ -15,9 +12,6 @@ in
   home.file."${profile}/permissions.sqlite".source = "${template}/permissions.sqlite";
   home.file."${profile}/search.json.mozlz4".source = "${template}/search.json.mozlz4";
 
-  # -------------------------
-  # Extensions (directory)
-  # -------------------------
   home.file."${profile}/extensions" = {
     source = "${template}/extensions";
     recursive = true;
@@ -27,27 +21,17 @@ in
   home.file."${profile}/extensions.json".source = "${template}/extensions.json";
   home.file."${profile}/extension-preferences.json".source = "${template}/extension-preferences.json";
 
-  # -------------------------
-  # Bookmarks + history
-  # -------------------------
   home.file."${profile}/places.sqlite".source = "${template}/places.sqlite";
   home.file."${profile}/favicons.sqlite".source = "${template}/favicons.sqlite";
 
-  # -------------------------
-  # Passwords
-  # -------------------------
   home.file."${profile}/logins.json".source = "${template}/logins.json";
   home.file."${profile}/logins-backup.json".source = "${template}/logins-backup.json";
   home.file."${profile}/key4.db".source = "${template}/key4.db";
   home.file."${profile}/cert9.db".source = "${template}/cert9.db";
 
-  # -------------------------
-  # Cookies + site storage
-  # -------------------------
   home.file."${profile}/cookies.sqlite".source = "${template}/cookies.sqlite";
   home.file."${profile}/webappstore.sqlite".source = "${template}/webappstore.sqlite";
 
-  # Safe persistent storage files
   home.file."${profile}/ls-archive.sqlite".source = "${template}/ls-archive.sqlite";
   home.file."${profile}/storage.sqlite".source = "${template}/storage.sqlite";
 }
