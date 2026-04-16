15457ec9d22a8af095b3dbf55b7efb59a9aa4ad8
Zack-4784 <trydster@hotmail.com>
2026-04-16 15:15:50 +0100

    AUTOMATED REBUILD

--- Home-Manager/Program-Configurations/kde.nix
+++ Home-Manager/Program-Configurations/kde.nix
@@ -1,22 +1,38 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  setWallpaper = wallpaper: pkgs.writeShellScript "set-wallpaper" ''
+    sleep 3
+    qdbus6 org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
+    var allDesktops = desktops();
+    for (i=0; i < allDesktops.length; i++) {
+      d = allDesktops[i];
+      d.wallpaperPlugin = 'org.kde.image';
+      d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+      d.writeConfig('Image', 'file://${wallpaper}');
+    }"
+  '';
+in {
   programs.plasma = {
     enable = true;
-    workspace.wallpaper = "/home/zack/Pictures/wallpapers/xenia.png";
     hotkeys.commands = {
       switch-to-swirls = {
         name = "Switch to Swirls Wallpaper";
-        key = "Meta+X";
-        command = "${pkgs.writeShellScript "set-swirls" ''
-          qdbus6 org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
-          var allDesktops = desktops();
-          for (i=0; i < allDesktops.length; i++) {
-            d = allDesktops[i];
-            d.wallpaperPlugin = 'org.kde.image';
-            d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
-            d.writeConfig('Image', 'file:///home/zack/Pictures/wallpapers/swirls.jpg');
-          }"
-        ''}";
+        key = "Meta+W";
+        command = "${setWallpaper "/home/zack/Pictures/wallpapers/swirls.jpg"}";
       };
+    };
+  };
+
+  systemd.user.services.set-wallpaper = {
+    Unit = {
+      Description = "Set wallpaper on login";
+      After = [ "plasma-plasmashell.service" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${setWallpaper "/home/zack/Pictures/wallpapers/xenia.png"}";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
     };
   };
 }
