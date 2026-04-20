55034d7b471f47488166d5ceb7f65a7bc83df11a
Zack-4784 <trydster@hotmail.com>
2026-04-20 09:59:48 +0100

    AUTOMATED REBUILD

--- Home-Manager/Program-Configurations/wallpaper-switcher.nix
+++ Home-Manager/Program-Configurations/wallpaper-switcher.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: let
   cycleWallpaper = pkgs.writeShellScriptBin "cycle-wallpaper" ''
     XENIA="/home/zack/Pictures/wallpapers/xenia.png"
+    BALCONIES="/home/zack/Pictures/wallpapers/balconies.jpg"
     SWIRLS="/home/zack/Pictures/wallpapers/swirls.jpg"
     STATE_FILE="/tmp/current-wallpaper"
 
@@ -13,6 +14,9 @@
     if [ "$CURRENT" = "xenia" ]; then
       plasma-apply-wallpaperimage "$SWIRLS"
       echo "swirls" > "$STATE_FILE"
+    elif [ "$CURRENT" = "swirls" ]; then
+      plasma-apply-wallpaperimage "$BALCONIES"
+      echo "balconies" > "$STATE_FILE"
     else
       plasma-apply-wallpaperimage "$XENIA"
       echo "xenia" > "$STATE_FILE"
